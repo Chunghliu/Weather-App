51ebdbe53b5d2a932d511fd185b93898a1dfa16d
Chunghliu <liuchunghin@gmail.com>
2021-10-20 00:16:05 +0800

    1)Updated some typo 2)Removed unused code 3)Added forgot-password link route

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,21 +3,16 @@ import React, { useState, useRef } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
-export default function Signup() {
+export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { login } = useAuth
+    const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
 
     async function handleSubmit(e) {
         e.preventDefault()
-
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Passowrds do not match')
-        }
 
         try {
             setError("")
@@ -35,7 +30,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="tect-center mb-4">Log In</h2>
+                    <h2 className="text-center mb-4">Log In</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
@@ -46,14 +41,13 @@ export default function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
                         <Button disabled={loading} className="w-100" type="submit">
                            Log In
                         </Button>
                     </Form>
+                    <div className="w-100 text-center mt-3">
+                        <Link to="/forgot-password">Forgot Password</Link>
+                    </div>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
